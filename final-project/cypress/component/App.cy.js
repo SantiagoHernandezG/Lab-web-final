be6211f6b50a3c060d95e69a9e3655104a7c563e
SantiagoHernandezG <Santiagohernandez2@icloud.com>
2022-12-01 21:07:50 -0600

    Editable text and cypress unit test

---
+++ final-project/cypress/component/App.cy.js
@@ -0,0 +1,15 @@
+import App from "../src/App.js";
+
+describe('App.cy.js', () => {
+  it('be visible', () => {
+    cy.viewport(1920, 1080)
+    cy.mount(<App/>)
+    cy.get('[data-cy=textarea]').should('be.visible')
+  })
+
+  it('edit text', () => {
+    cy.viewport(1920, 1080)
+    cy.mount(<App/>)
+    cy.get('input[value*="Puedes editar este texto dando click en el!"]').click().type(" Pong")
+  })
+})
--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {Card, Form} from "react-bootstrap";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [text, setText] = useState('Puedes editar este texto dando click en el!')
+    return (
+        <div className="App bg">
+            <Card className=" App-header">
+                <Card.ImgOverlay>
+                    <Card.Title>Santiago Hernández Guerrero</Card.Title>
+                    <Form >
+                    <span data-cy="textarea">
+                        <Form.Control
+                            required
+                            onChange={e => setText(e.target.value)}
+                            style={{fontSize: "50px",
+                                    width: "60vw",
+                                    marginTop: "30px"
+                            }}
+                            defaultValue={text}/>
+                    </span>
+                    </Form>
+                </Card.ImgOverlay>
+            </Card>
+        </div>
+    );
 }
 
 export default App;
